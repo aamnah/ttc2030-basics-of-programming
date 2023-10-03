1d9783de343471c77840398eda01bc5d2226f8c7
Amna <ad9795@student.jamk.fi>
2023-10-03 13:33:02 +0300

    + functions

--- examples/functions/functions.py
+++ examples/functions/functions.py
@@ -16,6 +16,15 @@ Procedure:
 a function that does not return anything
 '''
 
+# import a function from a different file
+from helper import subtract
+from nested/helper import multiply
+
+# stack overflow is caused by recurssion
+# you kept calling the same function, it kept allocating new memory for it
+# and then it runs out of stack memory and crashes
+# this doom event is called Stack Overflow
+
 random_number = 33
 
 
@@ -53,3 +62,21 @@ def lets_access_global_vars():
   print(random_number, 'is a global variable')
 
 lets_access_global_vars()
+
+# The text parameter of the modify_text function is its own copy.
+def modify_text(text):
+  text += ', this text is added by the function'
+  print(text)
+
+text = 'About to call modify_text'
+modify_text(text)
+print(text)
+
+
+# all basic types are passed as a copy when passed as an input
+# inside an object is passed by reference
+
+subtracted_number = subtract(10, 5)
+
+
+multiplied_number = multiply(10, 5)

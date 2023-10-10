ce9d9549a9947d74efc5493e54a3e7f7636f3bc3
Amna <ad9795@student.jamk.fi>
2023-10-10 13:26:51 +0300

    Classes, notes

--- examples/classes/classes.py
+++ examples/classes/classes.py
@@ -4,16 +4,27 @@ Classes can give us our own custom data strutcures
 
 from vehicle import Vehicle
 
-datsun = Vehicle()
-
+# datsun = Vehicle()
+datsun = Vehicle('Datsun', '100A', 998, 12)
 
 print(datsun) #<vehicle.Vehicle object at 0x000001F410C9E490>
 
-datsun.make = 'Datsun'
+""" datsun.make = 'Datsun'
 datsun.model = '100A'
 datsun.engine_cc = 998
-datsun.power_kw = 12
+datsun.power_kw = 12 """
+
+
 
 print(datsun.make) #Datsun
 
 # Make another vehicle, say Toyota
+
+toyota = Vehicle('Toyota', 'Celica', 1588, 43)
+
+print(toyota)
+
+print('Datsun power is:', datsun.horsepower())
+print('Toyota power is:', toyota.horsepower())
+
+print('\n')

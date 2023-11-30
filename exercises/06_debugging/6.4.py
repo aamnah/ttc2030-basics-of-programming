c08fb22ce1e692055522d554240207594e47c246
Amna <akram.aamnah@gmail.com>
2023-11-30 21:06:48 +0200

    tested

--- exercises/06_debugging/6.4.py
+++ exercises/06_debugging/6.4.py
@@ -14,3 +14,22 @@ Example output:
   Give points from judge 5: 20
   Total points are: 57
 """
+
+scores = []
+total_points = 0
+
+for i in range (5): # range index starts from 0
+  score = int(input(f"Give points from judge {i + 1}: "))
+  scores.append(score)
+
+# sort the array so that we have the numbers in order
+## methods like insert, remove or sort that only modify the list have no return value printed – they return the default None. 
+## This is a design principle for all mutable data structures in Python.
+scores.sort()
+
+# Loop over the item excluding the min/max values at start/end of the sorted array
+for i in scores[1:4]: # start index included, end index excluded
+  total_points+=i
+
+print(f"Total points are: {total_points}")
+

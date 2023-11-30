b0905b5b3a19a50ee86d5806397d8926f5bca57c
AD9795 <ad9795@student.jamk.fi>
2023-11-30 17:38:43 +0200

    ~ comments, float() placement

--- exercises/05_functions/consumption.py
+++ exercises/05_functions/consumption.py
@@ -1,7 +1,30 @@
+# Exercise 5: Functions
+# https://gitlab.labranet.jamk.fi/ttc2030/basics-of-programming/-/blob/master/exercises/exercise05.md
+
+"""
+5.3.
+Add function calc_consumption into the 'utils.py' file. calc_consumption takes parameters:
+
+Vehicle fuel consumption as liters per 100km
+Cost of fuel per liter
+Traveled distance in kilometres.
+
+The calc_consumption function prints to the console how many liters of fuel have been used on the way, as well as the amount of money spent on fuel.
+Create a file 'consumption.py' asking the user: consumption, fuel price and distance traveled. Then call the calc_consumption function from the program. Check that the function calculates consumption and fuel price correctly.
+
+Example output:
+
+  Enter trip length in km: 150
+  Enter fuel price/liter: 1.67
+  Enter fuel consumption/100 km: 4.
+  Fuel consumed: 6.30 liter
+  Cost: 10.52 €
+"""
+
 from utils import calc_consumption
 
-travel_distance_km = float(input("\nEnter trip length in km: "))
-fuel_cost = float(input("Enter fuel price/lite: "))
-fuel_consumption = float(input("Enter fuel consumption/100 km: "))
+travel_distance_km = input("\nEnter trip length in km: ")
+fuel_cost = input("Enter fuel price/lite: ")
+fuel_consumption = input("Enter fuel consumption/100 km: ")
 
 calc_consumption(travel_distance_km, fuel_cost, fuel_consumption)
--- exercises/05_functions/utils.py
+++ exercises/05_functions/utils.py
@@ -1,9 +1,20 @@
+# Exercise 5: Functions
+# https://gitlab.labranet.jamk.fi/ttc2030/basics-of-programming/-/blob/master/exercises/exercise05.md
+
+"""
+5.1.
+Create new source file 'utils.py'.
+Implement function show_info, which prints into the console "I'm Utils.Info"
+Create another new source file 'tester.py', where you call function `show_info`. Verify that output of the show_info is printed to the console.
+"""
+
 def show_info():
     print("I'm Utils.Info")
 
 def subtract(x, y):
+    # subtracting floats gives a float with a lot of decimal points
+    # e.g. 13.9 - 2.3 = 11.600000000000001
     result=round((x - y), 2) # round off to 2 decimal point
-    # 6.14 after rounding, otherwise 6.1434999999999995
     print(f"{x} - {y} is: {result}")
 
 def average(x, y, z):
@@ -23,8 +34,28 @@ def calc_consumption(travel_distance_km, fuel_cost, fuel_consumption):
         Traveled distance in kilometres
     """
 
+    # Make sure the user provided floating point numbers (and not strings)
+    # if not. cast input as float
+    if type(fuel_consumption) is not float:
+        fuel_consumption = float(fuel_consumption)
+
+    if type(fuel_price) is not float:
+        fuel_price = float(fuel_price)
+
+    if type(distance_traveled) is not float:
+        distance_traveled = float(distance_traveled)
+
+    # Do the math
     fuel_consumed = round(((travel_distance_km / 100) * fuel_consumption), 2)
     cost = round((fuel_consumed * fuel_cost), 2)
+
+    # Rounding floats can be done with round() as well as formatted strings
+    # See: https://stackoverflow.com/a/6539677
+    # for an explanation on unpredictable behaviour when using round() in Python
+    # round(2.675, 2) gives 2.67 rather than 2.68 
+    # Since Python 3.6 you can use .foramt() like below
+    # print('Fuel consumed:', '{:.2f}'.format(fuel_consumed), 'liter')
+    # print('Cost:', '{:.2f}'.format(cost), '€')
 
     print(f"""
 Fuel consumed: {fuel_consumed} liter
